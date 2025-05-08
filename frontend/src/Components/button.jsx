9ffc947a883682616d8a0b2f895502627a1de2e6
Azumeir <DominicAcosta455@gmail.com>
2025-05-08 14:39:12 +0800

    updated abit for testing

--- frontend/src/Components/button.jsx
+++ frontend/src/Components/button.jsx
@@ -5,8 +5,15 @@ function Button({ url = 'https://www.google.com', text = 'Visit this Website!'})
         window.location.href = url;
     };
 
+    const faviconUrl = `https://icons.duckduckgo.com/ip3/${new URL(url).hostname}.ico`;
+
     return (
         <button className = 'btn' onClick={handleClick}>
+            <img 
+                src={faviconUrl} 
+                alt="" 
+                style={{height: "20px", marginRight: "8px", verticalAlign: "middle"}}
+            />
             {text}
         </button>
     )
